18fd761d5270afefcbbc26f5cf71e82d132b980f
SheidakMaryia <mashasheidak@mail.ru>
2021-10-10 20:50:12 +0300

    correct DTOs

--- src/main/java/by/tms/dto/OperationDTO.java
+++ src/main/java/by/tms/dto/OperationDTO.java
@@ -2,20 +2,10 @@ package by.tms.dto;
 
 import by.tms.entity.User;
 import by.tms.utils.ErrorsMessageManager;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
 public class OperationDTO {
 
     @NotNull(message = ErrorsMessageManager.NUMBER_ERROR)
